829478726c240a38a2e3f0cb607ca5617a6e2ba1
rbachlani <raheel.bachlani@cooperativecomputing.com>
2020-11-27 04:17:50 +0500

    zxzxzxzxzxxzxz

--- TestCases/browseropen.py
+++ TestCases/browseropen.py
@@ -3,4 +3,3 @@ from selenium import webdriver
 class test_chromebrowser:
     driver2=webdriver.Chrome()
     driver2.get("https://www.saspeeasdsadadtest.net/")
-driver2=webdriver.Chrome()driver2=webdriver.Chrome()driver2=webdriver.Chrome()driver2=webdriver.Chrome()driver2=webdriver.Chrome()driver2=webdriver.Chrome()
